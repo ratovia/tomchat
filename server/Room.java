37beb097f7941c2b8a1a20758f2723ace7f63753
ratovia <ratovia@hotmail.co.jp>
2017-03-03 07:23:48 +0900

    [update] room method
    constractor and method skeleton

--- server/Room.java
+++ server/Room.java
@@ -1,3 +1,23 @@
-public class Room {
+import java.util.ArrayList;
 
+public class Room {
+  private String name;
+  private ArrayList<User> users;
+
+  public Room(String name){
+    this.name = name;
+    users = new ArrayList<User>();
+  }
+
+  public void connect(){
+
+  }
+
+  public void leave(){
+
+  }
+
+  public void close(){
+
+  }
 }
