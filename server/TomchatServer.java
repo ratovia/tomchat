ddd46bf22e32b13b515b81c4790e1e3de27e8428
ratovia <ratovia@hotmail.co.jp>
2017-03-14 23:44:32 +0900

    [add] クライアントを接続するソケットを用意

--- server/TomchatServer.java
+++ server/TomchatServer.java
@@ -1,5 +1,29 @@
+import java.net.*;
+import java.io.*;
+
 class TomchatServer {
   public static void main(String args[]){
     System.out.println("start tomchat");
+    TomchatServer server = new TomchatServer();
+    server.start();
+  }
+
+  public void start(){
+    // サーバのソケットを用意
+    ServerSocket ss = null;
+    try{
+      ss = new ServerSocket(9999);
+    }catch(IOException e){
+      System.out.println(e);
+    }
+    // accept()でクライアントを待機
+    while(true){
+      Socket cs = null;
+      try{
+        cs = ss.accept();
+      }catch(IOException e){
+        System.out.println(e);
+      }
+    }
   }
 }
